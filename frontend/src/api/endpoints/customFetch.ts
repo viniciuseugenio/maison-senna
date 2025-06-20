5105d009d61bcb7956c5b7ed58cdf94a74665110
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-06-20 15:02:40 -0300

    feat(custom-fetch): transform keys coming from the back-end to camelCase
    - As the keys from the back-end always come as snake_case, and in the front-end we use camelCase,
    the `customFetch` function will automatically make this conversion

--- frontend/src/api/endpoints/customFetch.ts
+++ frontend/src/api/endpoints/customFetch.ts
@@ -1,8 +1,9 @@
 import { UNEXPECTED_ERROR } from "./constants";
+import humps from "humps";
 
 export async function customFetch(
   url: string,
-  options?: Record<string, unknown>,
+  options?: RequestInit,
   ignore400?: boolean,
 ) {
   try {
@@ -11,19 +12,20 @@ export async function customFetch(
       credentials: "include",
     });
     const data = await response.json();
+    const camelData = humps.camelizeKeys(data);
 
     if (!response.ok) {
       if (response.status === 400 && ignore400) {
-        return { errors: data, status: response.status };
+        return { errors: camelData, status: response.status };
       }
       throw {
-        title: data.detail || UNEXPECTED_ERROR,
-        description: data.description,
+        title: camelData.detail || UNEXPECTED_ERROR,
+        description: camelData.description,
         status: response.status,
       };
     }
 
-    return data;
+    return camelData;
   } catch (error: unknown) {
     if (
       typeof error === "object" &&
