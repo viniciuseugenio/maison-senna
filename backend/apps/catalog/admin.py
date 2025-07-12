d07494467e03b1d7dc2bc32cb0b54efe968a2536
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-07-12 17:07:56 -0300

    chore(django-admin): register models

--- backend/apps/catalog/admin.py
+++ backend/apps/catalog/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+
+from . import models
+
+admin.site.register(models.VariationKind)
+admin.site.register(models.Category)
+admin.site.register(models.Product)
+admin.site.register(models.VariationOption)
+admin.site.register(models.VariationType)
+admin.site.register(models.ProductVariation)
