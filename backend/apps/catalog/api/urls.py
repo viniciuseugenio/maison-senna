2de2c3c8d6c133fcfbfbee9ecf128e5a6e133eb0
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-07-19 16:39:13 -0300

    refactor(catalog): update URL names to match Django routers URL names

--- backend/apps/catalog/api/urls.py
+++ backend/apps/catalog/api/urls.py
@@ -11,32 +11,32 @@ urlpatterns = [
     path(
         "categories/",
         views.CategoryListCreateView.as_view(),
-        name="categories_list",
+        name="categories-list",
     ),
     path(
         "categories/<int:pk>/",
         views.CategoryDetailsView.as_view(),
-        name="categories_details",
+        name="categories-detail",
     ),
     path(
         "variation/kinds/",
         views.VariationKindsList.as_view(),
-        name="variation_kinds_list",
+        name="variation-kinds-list",
     ),
     path(
         "variation/types/",
         views.VariationTypesList.as_view(),
-        name="variation_types_list",
+        name="variation-types-list",
     ),
     path(
         "variation/options/",
         views.VariationOptionsList.as_view(),
-        name="variation_options_list",
+        name="variation-options-list",
     ),
     path(
         "products_variations/",
         views.ProductVariationList.as_view(),
-        name="product_variations_list",
+        name="product-variations-list",
     ),
 ]
 
