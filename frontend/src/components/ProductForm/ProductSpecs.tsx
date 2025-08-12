8b62020c89857c313822e1fe71fae9c262c577b9
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-08-12 12:03:55 -0300

    style(product-specs): update border color on error

--- frontend/src/components/ProductForm/ProductSpecs.tsx
+++ frontend/src/components/ProductForm/ProductSpecs.tsx
@@ -4,6 +4,7 @@ import { useFormContext } from "react-hook-form";
 import InputError from "../InputError";
 import ProductSpecItem from "./ProductSpecItem";
 import { ProductSpecsProps } from "./types";
+import { twMerge } from "tailwind-merge";
 
 const ProductSpecs: React.FC<ProductSpecsProps> = ({
   name,
@@ -53,7 +54,10 @@ const ProductSpecs: React.FC<ProductSpecsProps> = ({
           id={name}
           aria-invalid={!!error}
           placeholder={placeholder}
-          className="border-oyster/20 focus:border-oyster ring-oyster/30 flex-1 rounded-md border bg-white p-2 text-sm duration-300 outline-none focus:ring-2"
+          className={twMerge(
+            "border-oyster/20 focus:border-oyster ring-oyster/30 flex-1 rounded-md border bg-white p-2 text-sm duration-300 outline-none focus:ring-2",
+            error && "border-red-500 ring-red-300 focus:border-red-600",
+          )}
         />
         <button
           onClick={onAdd}
