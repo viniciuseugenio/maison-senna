e557fcd880c06a542fb4bae445b629dc3c28e6c0
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-12-16 21:19:56 -0300

    fix(unauthenticated-routes): remove redundant toast notification on redirect

--- frontend/src/components/UnauthenticatedRoutes.tsx
+++ frontend/src/components/UnauthenticatedRoutes.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router";
 import { useAuth } from "../store/useAuth";
-import { toast } from "../utils/customToast";
 
 const UnauthenticatedRoutes: React.FC = () => {
   const { isAuthenticated } = useAuth();
@@ -9,11 +8,6 @@ const UnauthenticatedRoutes: React.FC = () => {
 
   useEffect(() => {
     if (isAuthenticated) {
-      toast.error({
-        title: "You cannot enter this page.",
-        description:
-          "This page is meant only for users that are not authenticated.",
-      });
       navigate("/");
     }
   }, [isAuthenticated, navigate]);
