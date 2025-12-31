a70d315a3fc304c40b0e5a95e2bca09a3de99de4
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2025-12-31 11:40:41 -0300

    chore: remove obsolete `typeGuards` file

--- frontend/src/utils/typeGuards.ts
+++
@@ -1,7 +0,0 @@
-import { ApiFormError, ApiResponse } from "../types/api";
-
-export function isApiFormError(
-  data: ApiFormError | ApiResponse,
-): data is ApiFormError {
-  return "errors" in data;
-}
