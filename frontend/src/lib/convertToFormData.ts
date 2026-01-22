7214fec4c170a31693d8e4800ecdb9e5aa47fb29
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-01-22 14:26:55 -0300

    fix(convert-to-formdata): update naming of the keys to match back-end

--- frontend/src/lib/convertToFormData.ts
+++ frontend/src/lib/convertToFormData.ts
@@ -2,15 +2,17 @@ import { SpecItem } from "@components/features/product-form/types";
 import { NewProductForm } from "@/types/forms";
 
 type VariationsObj = {
-  id: string;
-  variationKind: number;
+  idx: string;
+  kind: number;
   options: SpecItem[];
 };
 
 function formatVariations(variations: VariationsObj[]) {
   const newVariations = variations.map((variation) => ({
-    variationKind: variation.variationKind,
-    options: variation.options.map((option) => option.value),
+    kind: variation.kind,
+    options: variation.options.map((option) => ({
+      name: option.name,
+    })),
   }));
   return newVariations;
 }
@@ -18,13 +20,16 @@ function formatVariations(variations: VariationsObj[]) {
 export function convertToFormData(data: NewProductForm): FormData {
   const formData = new FormData();
   formData.set("name", data.name);
-  formData.set("base_price", String(data.basePrice));
-  formData.set("reference_image", data.referenceImage);
+  formData.set("basePrice", String(data.basePrice));
+  formData.set("referenceImage", data.referenceImage);
   formData.set("description", data.description);
   formData.set("category", String(data.category));
   formData.set("details", JSON.stringify(data.details));
   formData.set("materials", JSON.stringify(data.materials));
   formData.set("care", JSON.stringify(data.care));
-  formData.set("variations", JSON.stringify(formatVariations(data.variations)));
+  formData.set(
+    "variationOptions",
+    JSON.stringify(formatVariations(data.variationOptions)),
+  );
   return formData;
 }
