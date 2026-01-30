eb0d8b8658453b4998bd054281a23162bf51b12a
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-01-29 21:53:52 -0300

    fix(layout): wrap only the `Outlet` with main element

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -11,10 +11,12 @@ export default function Layout() {
   }, [pathname]);
 
   return (
-    <main className="min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <Navbar />
-      <Outlet />
+      <main className="flex-1">
+        <Outlet />
+      </main>
       <Footer />
-    </main>
+    </div>
   );
 }
