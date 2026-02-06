bfb346dd1b1098050b928fe2b9cd8b71d8870ddb
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-02-06 20:21:57 -0300

    feat(admin): register `WishlistItem` model

--- backend/apps/catalog/admin.py
+++ backend/apps/catalog/admin.py
@@ -8,6 +8,11 @@ admin.site.register(models.VariationOption)
 admin.site.register(models.ProductVariation)
 
 
+@admin.register(models.WishlistItem)
+class WishlistItemAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "product", "added_at"]
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "slug", "base_price", "is_featured"]
