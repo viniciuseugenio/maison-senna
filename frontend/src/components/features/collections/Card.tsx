fa1a66372b589227008b8719304a11acf68eba54
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-02-28 14:24:03 -0300

    feat(collections): wrap image in Link element

--- frontend/src/components/features/collections/Card.tsx
+++ frontend/src/components/features/collections/Card.tsx
@@ -9,21 +9,25 @@ type CardProps = {
 };
 
 const Card: React.FC<CardProps> = ({ className, img, title, slug }) => {
+  const cardLink = `/collections/${slug}`;
+
   return (
     <div className={twMerge(className, "group cursor-pointer")}>
-      <div className="mb-6 aspect-[4/5] overflow-hidden">
-        <img
-          className="transition-tranform h-full w-full object-cover duration-300 group-hover:scale-105"
-          src={img}
-        />
-      </div>
+      <Link to={cardLink}>
+        <div className="mb-6 aspect-[4/5] overflow-hidden">
+          <img
+            className="transition-tranform h-full w-full object-cover duration-300 group-hover:scale-105"
+            src={img}
+          />
+        </div>
+      </Link>
       <div className="flex flex-col items-center text-center">
         <p className="text-oyster mb-3 text-xs tracking-[.3em] uppercase">
           Timeless Elegance
         </p>
         <h3 className="text-mine-shaft mb-6 font-serif text-3xl">{title}</h3>
         <Link
-          to={`/collections/${slug}`}
+          to={cardLink}
           className="bg-mine-shaft hover:bg-mine-shaft/95 text-light mx-auto px-14 py-4 text-sm font-light tracking-widest uppercase duration-300"
         >
           Discover
