a100ba6eb43d7bbd2a2e39aa5796b739fef91b6c
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-03-02 15:32:56 -0300

    feat(debug-toolbar): add debug toolbar to track SQL queries

--- backend/CORE/urls.py
+++ backend/CORE/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -27,30 +28,34 @@ from drf_spectacular.views import (
 
 from apps.accounts.api.views import CustomTokenObtainPairView, CustomTokenRefreshView
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/v1/accounts/", include("apps.accounts.api.urls")),
-    path("api/v1/catalog/", include("apps.catalog.api.urls")),
-    path(
-        "api/v1/token/",
-        CustomTokenObtainPairView.as_view(),
-        name="token_obtain_pair",
-    ),
-    path(
-        "api/v1/token/refresh/",
-        CustomTokenRefreshView.as_view(),
-        name="token_refresh",
-    ),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    # Optional UI:
-    path(
-        "api/schema/swagger-ui/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
-    path(
-        "api/schema/redoc/",
-        SpectacularRedocView.as_view(url_name="schema"),
-        name="redoc",
-    ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("api/v1/accounts/", include("apps.accounts.api.urls")),
+        path("api/v1/catalog/", include("apps.catalog.api.urls")),
+        path(
+            "api/v1/token/",
+            CustomTokenObtainPairView.as_view(),
+            name="token_obtain_pair",
+        ),
+        path(
+            "api/v1/token/refresh/",
+            CustomTokenRefreshView.as_view(),
+            name="token_refresh",
+        ),
+        path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+        # Optional UI:
+        path(
+            "api/schema/swagger-ui/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
+        path(
+            "api/schema/redoc/",
+            SpectacularRedocView.as_view(url_name="schema"),
+            name="redoc",
+        ),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + debug_toolbar_urls()
+)
