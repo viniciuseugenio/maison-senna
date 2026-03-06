cd8c11c68ab6f813f9d3360057f872f7c7aed0c7
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-03-05 23:11:45 -0300

    build: remove pydantic and use TypeDict

--- backend/apps/catalog/services/variation_service.py
+++ backend/apps/catalog/services/variation_service.py
@@ -1,18 +1,18 @@
 from django.db import IntegrityError
-from pydantic import BaseModel
 from rest_framework import serializers
+from typing_extensions import TypedDict
 
 from apps.catalog import models
 
 
-class OptionObject(BaseModel):
+class OptionObject(TypedDict):
     id: str
     name: str
     price_modifier: str
 
 
-class VariationOptions(BaseModel):
-    kind: str
+class VariationOptions(TypedDict):
+    kind: models.VariationKind
     options: list[OptionObject]
 
 
@@ -20,16 +20,16 @@ def create_variation_options(
     variation_options: list[VariationOptions],
     product_instance: models.Product,
 ):
-    for index, option_data in enumerate(variation_options):
-        kind = option_data.kind
 
-        for option in option_data.options:
-            id = option.id
-            if id:
+    for index, option_data in enumerate(variation_options):
+        kind = option_data.get("kind")
+
+        for option in option_data.get("options"):
+            if option.get("id"):
                 continue
 
-            option_name = option.name
-            price_modifier = option.price_modifier
+            option_name = option.get("name")
+            price_modifier = option.get("price_modifier")
 
             try:
                 models.VariationOption.objects.create(
