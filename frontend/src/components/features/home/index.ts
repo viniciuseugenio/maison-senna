a5650e0f1c60afedc81d7dc5b7c5544af47747ad
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-03-10 10:39:38 -0300

    refactor(home): create `index.ts` and barrel export

---
+++ frontend/src/components/features/home/index.ts
@@ -0,0 +1,4 @@
+export { default as FeaturedProducts } from "./FeaturedProducts";
+export { default as Hero } from "./Hero";
+export { default as ProjectShowcase } from "./ProjectShowcase";
+export { default as ProjectTool } from "./ProjectTool";
