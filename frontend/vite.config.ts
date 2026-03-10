b1864952021c0a5d59aa2577c7ef76757802baea
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-03-10 11:59:17 -0300

    feat: add path alias to `api/` folder

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       "@components": path.resolve(__dirname, "./src/components"),
       "@hooks": path.resolve(__dirname, "./src/hooks"),
       "@assets": path.resolve(__dirname, "./src/assets"),
+      "@api": path.resolve(__dirname, "./src/api/"),
     },
   },
   server: {
