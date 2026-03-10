441860fbecf584732c9b11600ea7f9c86bb0b281
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-03-10 10:31:11 -0300

    refactor(product-details): create `index.ts` and barrel export

---
+++ frontend/src/components/features/product-details/index.ts
@@ -0,0 +1,8 @@
+export { default as DetailsButton } from "./DetailsButton";
+export { default as DetailsTabs } from "./DetailsTabs";
+export { default as ProductReviewItem } from "./ProductReviewItem";
+export { default as ProductReviews } from "./ProductReviews";
+export { default as ProductVariations } from "./ProductVariations";
+export { default as TabContent } from "./TabContent";
+export { default as VariantButton } from "./VariantButton";
+export { default as WishlistButton } from "./WishlistButton";
