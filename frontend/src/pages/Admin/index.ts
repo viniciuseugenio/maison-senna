b6c6da0a4aeaebe9c7064d91c651897d65728fee
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-03-11 12:14:37 -0300

    feat(admin-pages): add barrel export to pages

---
+++ frontend/src/pages/Admin/index.ts
@@ -0,0 +1,7 @@
+export { default as Categories } from "./Categories";
+export { default as Dashboard } from "./Dashboard";
+export { default as Products } from "./Products";
+export { default as ProductVariations } from "./ProductVariations";
+export { default as VariationKinds } from "./VariationKinds";
+export { default as VariationOptionModal } from "./VariationOptionModal";
+export { default as VariationOptions } from "./VariationOptions";
