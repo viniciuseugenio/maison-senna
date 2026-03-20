4dba888d584f8bb53641473fbbacb334fa3b55eb
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-03-19 22:31:14 -0300

    refactor(layout): remove manual scroll to top

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,15 +1,7 @@
-import { useLocation } from "@tanstack/react-router";
-import { useEffect } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
