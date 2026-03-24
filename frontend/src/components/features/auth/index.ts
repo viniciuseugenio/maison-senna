eaadae0cd60f329d1da6aef5d9f79af8d17ba2e4
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-03-24 15:26:25 -0300

    refactor(routes): remove obsolete files
    These deleted files were used in the old routing feature, with
    react-router

--- frontend/src/components/features/auth/index.ts
+++ frontend/src/components/features/auth/index.ts
@@ -2,7 +2,5 @@ export { default as AuthButtons } from "./AuthButtons";
 export { default as FloatingInputPassword } from "./FloatingInputPassword";
 export { default as LoginPasswordInput } from "./LoginPasswordInput";
 export { default as PasswordRequirement } from "./PasswordRequirement";
-export { default as ProtectedRoute } from "./ProtectedRoute";
 export { default as RegisterPasswordInputs } from "./RegisterPasswordInputs";
 export { default as SocialLogin } from "./SocialLogin";
-export { default as UnauthenticatedRoutes } from "./UnauthenticatedRoutes";
--- frontend/src/pages/Admin/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ProductVariations } from "./ProductVariations";
-export { default as VariationKinds } from "./VariationKinds";
-export { default as VariationOptionModal } from "./VariationOptionModal";
-export { default as VariationOptions } from "./VariationOptions";
