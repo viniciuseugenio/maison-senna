e5108aa430f6c3db096da9a5655622dcdb12ef0a
Vinicius Eugênio <viniciuseugeniovhe@gmail.com>
2026-04-28 10:26:16 -0300

    fix(table-data): use twMerge correctly

--- frontend/src/components/features/admin/TableData.tsx
+++ frontend/src/components/features/admin/TableData.tsx
@@ -3,11 +3,12 @@ import { twMerge } from "tailwind-merge";
 const TableData: React.FC<{
   children: React.ReactNode;
   className?: string;
-}> = ({ children, className }) => {
+}> = ({ children, className = "" }) => {
   return (
     <td
       className={twMerge(
-        `text-mine-shaft px-6 py-4 text-sm whitespace-nowrap ${className ?? ""}`,
+        "text-mine-shaft px-6 py-4 text-sm whitespace-nowrap",
+        className,
       )}
     >
       {children}
